3850687a66b266e006e9ac1488f0030385fa46e9
Mohosin999 <programmermohosin@gmail.com>
2023-06-17 15:43:31 +0600

    initial commit

--- app/page.js
+++ app/page.js
@@ -1,12 +1,9 @@
-"use client";
 import React from "react";
-import Button from "./components/button/Button";
 
 const Home = () => {
   return (
     <div>
       <h2>Root Page</h2>
-      <Button />
     </div>
   );
 };
