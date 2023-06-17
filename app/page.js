2e57cff7ac3274bd8c0d9fcf05ccce2b5c148056
Mohosin999 <programmermohosin@gmail.com>
2023-06-17 21:38:02 +0600

    initial commit

--- app/page.js
+++ app/page.js
@@ -1,7 +1,10 @@
+"use client";
 import React from "react";
 import useClock from "./hooks/use-clock/useClock";
 
 const Home = () => {
+  const { date, dateUtc, offset, timezone } = useClock();
+
   return (
     <div>
       <h2>Root Page</h2>
--- jest.config.js
+++ jest.config.js
@@ -12,6 +12,7 @@ const customJestConfig = {
     "^@/components/(.*)$": "<rootDir>/components/$1",
   },
   testEnvironment: "jest-environment-jsdom",
+  resetMocks: true,
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
