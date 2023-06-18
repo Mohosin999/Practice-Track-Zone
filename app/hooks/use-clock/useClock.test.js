ea0f84d0a5ce7e64c939ec964d64ad4b8417358d
Mohosin999 <programmermohosin@gmail.com>
2023-06-18 08:27:26 +0600

    initial commit

--- app/hooks/use-clock/useClock.test.js
+++ app/hooks/use-clock/useClock.test.js
@@ -2,60 +2,72 @@ import { renderHook } from "@testing-library/react-hooks";
 import { addMinutes, startOfMinute } from "date-fns";
 import useClock from "./useClock";
 
+// jest.mock("date-fns");
+
 describe("useClock", () => {
   afterEach(() => {
     jest.resetAllMocks();
   });
 
-  it("should initialize correctly with default values", () => {
-    const { result } = renderHook(() => useClock());
+  it("should set default local date based on local timezone and offset", () => {
+    const { result, rerender } = renderHook(() => useClock());
 
-    const currentUtc = new Date();
-    const expectedDate = addMinutes(
-      startOfMinute(currentUtc),
-      -currentUtc.getTimezoneOffset()
-    );
-
-    expect(result.current.date.getTime()).toEqual(expectedDate.getTime());
-    expect(result.current.dateUtc).toBeNull();
-    expect(result.current.offset).toBe(0);
-    expect(result.current.timezone).toBeNull();
-  });
-
-  it("should set the UTC date and local offset on mount", () => {
-    // const mockDate = new Date("2023-06-17T12:00:00Z");
-    // const mockOffset = 240;
-    // jest.spyOn(global, "Date").mockImplementation(() => mockDate);
-    // addMinutes.mockReturnValueOnce(addMinutes(mockDate, mockOffset));
-    // const { result } = renderHook(() => useClock());
-    // expect(result.current.dateUtc).toEqual(mockDate);
-    // expect(result.current.offset).toEqual(mockOffset);
+    expect(result.current.date).not.toBeNull();
+    expect(result.current.dateUtc).not.toBeNull();
+    expect(result.current.offset).not.toBe(0);
+    expect(result.current.timezone).not.toBeNull();
   });
 
   it("should update local date when timezone is provided", () => {
-    //   const mockUtc = new Date("2023-06-17T12:00:00Z");
-    //   const mockOffset = 300;
-    //   const mockTimezone = "PST";
-    //   const mockLocalDate = addMinutes(mockUtc, mockOffset);
-    //   addMinutes.mockReturnValueOnce(mockLocalDate);
-    //   const { result, rerender } = renderHook(
-    //     ({ timezone, offset }) => useClock(timezone, offset),
-    //     {
-    //       initialProps: { timezone: null, offset: null },
-    //     }
-    //   );
-    //   rerender({ timezone: mockTimezone, offset: null });
-    //   expect(result.current.date).toEqual(mockLocalDate);
-    //   expect(result.current.timezone).toEqual(mockTimezone);
-    // });
-    // it("should update local date when timezone is not provided", () => {
-    //   const mockUtc = new Date("2023-06-17T12:00:00Z");
-    //   const mockOffset = 300;
-    //   const mockLocalDate = addMinutes(mockUtc, -mockOffset);
-    //   const mockLocalTimezone = "PST";
-    //   addMinutes.mockReturnValueOnce(mockLocalDate);
-    //   const { result } = renderHook(() => useClock(null, null));
-    //   expect(result.current.date).toEqual(mockLocalDate);
-    //   expect(result.current.timezone).toEqual(mockLocalTimezone);
+    jest
+      .spyOn(Date.prototype, "toISOString")
+      .mockReturnValueOnce("2023-06-17T12:11:15.555Z");
+    const mockOffset = 300;
+    const mockTimezone = "PST";
+    const mockLocalDate = new Date("2023-06-17T12:11:15.555Z");
+
+    jest
+      .spyOn(Date.prototype, "getTimezoneOffset")
+      .mockReturnValueOnce(mockOffset);
+    addMinutes.mockReturnValueOnce(mockLocalDate);
+
+    const { result, rerender } = renderHook(
+      ({ timezone, offset }) => useClock(timezone, offset),
+      {
+        initialProps: { timezone: null, offset: null },
+      }
+    );
+
+    rerender({ timezone: mockTimezone, offset: null });
+
+    expect(result.current.date).toEqual(mockLocalDate);
+    expect(result.current.timezone).toEqual(mockTimezone);
+    // const mockUtc = new Date("2023-06-17T12:11:15.555Z");
+    // const mockOffset = 300;
+    // const mockTimezone = "PST";
+    // const mockLocalDate = addMinutes(mockUtc, mockOffset);
+
+    // const { result, rerender } = renderHook(
+    //   ({ timezone, offset }) => useClock(timezone, offset),
+    //   {
+    //     initialProps: { timezone: null, offset: null },
+    //   }
+    // );
+
+    // rerender({ timezone: mockTimezone, offset: null });
+
+    // expect(result.current.date).toEqual(mockLocalDate);
+    // expect(result.current.timezone).toEqual(mockTimezone);
+  });
+
+  it("should update local date when timezone is not provided", () => {
+    // const mockUtc = new Date("2023-06-17T12:00:00Z");
+    // const mockOffset = 300;
+    // const mockLocalDate = addMinutes(mockUtc, -mockOffset);
+    // const mockLocalTimezone = "PST";
+    // addMinutes.mockReturnValueOnce(mockLocalDate);
+    // const { result } = renderHook(() => useClock(null, null));
+    // expect(result.current.date).toEqual(mockLocalDate);
+    // expect(result.current.timezone).toEqual(mockLocalTimezone);
   });
 });
