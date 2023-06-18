acd4615f16548e765ac775731ffd285f945d9a79
Mohosin999 <programmermohosin@gmail.com>
2023-06-18 12:31:08 +0600

    test code for custom hook

--- app/hooks/use-clock/useClock.test.js
+++ app/hooks/use-clock/useClock.test.js
@@ -2,23 +2,53 @@ import { renderHook } from "@testing-library/react";
 import { addMinutes } from "date-fns";
 import useClock from "./useClock";
 
-jest.mock("date-fns");
+jest.mock("date-fns", () => ({
+  addMinutes: jest.fn(
+    (date, minutes) => new Date(date.getTime() + minutes * 60000)
+  ),
+}));
 
 describe("useClock", () => {
   afterEach(() => {
-    jest.resetAllMocks();
+    jest.clearAllMocks();
   });
 
-  it("should update local date when timezone is provided", () => {});
+  it("should update the local date and timezone when timezone is provided", () => {
+    const mockUtc = new Date("2023-06-18T05:36:07.371Z");
+
+    const mockOffset = 300;
+    const mockLocalDate = addMinutes(mockUtc, mockOffset);
+    const mockTimezone = "PST";
+
+    const { result, rerender } = renderHook((props) => useClock(...props), {
+      initialProps: [mockTimezone, null],
+    });
+
+    expect(result.current.date).toEqual(mockLocalDate);
+    expect(result.current.timezone).toEqual(mockTimezone);
+
+    const newOffset = 420;
+    const newLocalDate = addMinutes(mockUtc, newOffset);
+    const newTimezone = "EST";
+
+    rerender([newTimezone, null]);
+
+    expect(result.current.date).toEqual(newLocalDate);
+    expect(result.current.timezone).toEqual(newTimezone);
+  });
 
   it("should update local date when timezone is not provided", () => {
-    // const mockUtc = new Date("2023-06-17T12:00:00Z");
-    // const mockOffset = 300;
+    // let d = new Date();
+    // const lo = d.getTimezoneOffset();
+    // d = addMinutes(d, lo);
+    // const mockUtc = new Date("2023-06-18T05:36:07.371Z");
+    // const mockOffset = -360;
     // const mockLocalDate = addMinutes(mockUtc, -mockOffset);
-    // const mockLocalTimezone = "PST";
+    // const mockLocalTimezone = "GMT";
     // addMinutes.mockReturnValueOnce(mockLocalDate);
-    // const { result } = renderHook(() => useClock(null, null));
-    // expect(result.current.date).toEqual(mockLocalDate);
-    // expect(result.current.timezone).toEqual(mockLocalTimezone);
+    // const { result } = renderHook(() => useClock());
+    // expect(result.current.date).toBe(d);
+    // expect(result.current.timezone).toBe();
+    // expect(result.current.timezone).toEqual();
   });
 });
