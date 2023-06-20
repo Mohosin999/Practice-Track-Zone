66b0df5a44f5809dde1f942eb437d165daa0b7c8
Mohosin999 <programmermohosin@gmail.com>
2023-06-20 08:40:02 +0600

    commit

--- app/components/local-clock/LocalClock.jsx
+++ app/components/local-clock/LocalClock.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
+import useClock from "@/app/hooks/use-clock/useClock";
+import CLockDisplay from "../shared/clock-display/CLockDisplay";
 
-const LocalClock = () => {
-  return <div></div>;
+const LocalClock = ({ clock, updateClock }) => {
+  const { date, timezone, offset } = useClock(clock.timezone, clock.offset);
+
+  useEffect(() => {
+    updateClock({
+      date,
+      timezone,
+      offset,
+    });
+  }, [date]);
+
+  return <div>{date && <CLockDisplay date={date} />}</div>;
 };
 
 export default LocalClock;
