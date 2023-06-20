128fb38184b85e2d3b07c5d02ffd45a8d1cda433
Mohosin999 <programmermohosin@gmail.com>
2023-06-20 08:40:13 +0600

    commit

--- app/components/shared/clock-display/CLockDisplay.jsx
+++ app/components/shared/clock-display/CLockDisplay.jsx
@@ -1,19 +1,27 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { format } from "date-fns";
 
 const CLockDisplay = ({ date, title, timezone, offset }) => {
-  //   let offsetHr = offset / 60;
+  let offsetHr = offset / 60;
 
   return (
     <div>
       <h1>Title: {title}</h1>
       <h3>{format(date, "yyyy-MM-dd hh:mm:ss aaa")}</h3>
-      {/* <p>
+      <p>
         {timezone}
         {offsetHr > 0 ? `+${Math.abs(offsetHr)}` : `-${Math.abs(offsetHr)}`}
-      </p> */}
+      </p>
     </div>
   );
+};
+
+CLockDisplay.propTypes = {
+  date: PropTypes.instanceOf(Date).isRequired,
+  title: PropTypes.string.isRequired,
+  timezone: PropTypes.string.isRequired,
+  offset: PropTypes.number.isRequired,
 };
 
 export default CLockDisplay;
