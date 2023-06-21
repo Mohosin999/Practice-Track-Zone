31e081c3a950cdc4f431e651db68dbd055392302
Mohosin999 <programmermohosin@gmail.com>
2023-06-21 07:38:49 +0600

    test code

--- app/components/shared/clock-actions/ClockActions.test.js
+++ app/components/shared/clock-actions/ClockActions.test.js
@@ -1,3 +1,35 @@
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
 import ClockActions from "./ClockActions";
+
+describe("ClockActions", () => {
+  const clock = {
+    title: "Test Clock",
+    timezone: "GMT",
+    offset: -690,
+  };
+
+  const updateClock = jest.fn();
+
+  it("should renders correctly when local is false", () => {
+    render(
+      <ClockActions local={false} clock={clock} updateClock={updateClock} />
+    );
+
+    expect(screen.getByText("Edit")).toBeInTheDocument();
+    expect(screen.getByText("Delete")).toBeInTheDocument();
+  });
+
+  it("should renders correctly when local is true", () => {
+    render(
+      <ClockActions local={true} clock={clock} updateClock={updateClock} />
+    );
+
+    expect(screen.getByText("Edit")).toBeInTheDocument();
+    expect(screen.getByText("Create")).toBeInTheDocument();
+  });
+
+  it("should update clock on input change", () => {
+    render(<ClockActions clock={clock} updateClock={updateClock} />);
+  });
+});
