5e0541c51324313b6147677204d9c571b30d26dd
Mohosin999 <programmermohosin@gmail.com>
2023-06-23 08:30:54 +0600

    test code for clock actions

--- app/components/shared/clock-actions/ClockActions.test.js
+++ app/components/shared/clock-actions/ClockActions.test.js
@@ -9,7 +9,7 @@ jest.mock("../clock-form/ClockForm", () => {
       id={id}
       name={name}
       value={value}
-      handleClock={handleClock}
+      onChange={(e) => handleClock(e.target.value)}
       disabled={disabled}
     />
   ));
@@ -26,7 +26,11 @@ describe("ClockActions", () => {
   const createClock = jest.fn();
   const deleteClock = jest.fn();
 
-  it("should renders correctly when local is false", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should render correctly when local is false", () => {
     render(
       <ClockActions
         local={false}
@@ -41,7 +45,7 @@ describe("ClockActions", () => {
     expect(screen.getByText("Delete Folder")).toBeInTheDocument();
   });
 
-  it("should renders correctly when local is true", () => {
+  it("should render correctly when local is true", () => {
     render(
       <ClockActions
         local={true}
@@ -56,9 +60,133 @@ describe("ClockActions", () => {
     expect(screen.getByText("Create Folder")).toBeInTheDocument();
   });
 
-  it("should toggles clock form when clicked edit clock button", () => {});
+  it("should toggle clock form when clicked edit clock button", () => {
+    render(
+      <ClockActions
+        local={false}
+        clock={clock}
+        updateClock={updateClock}
+        createClock={createClock}
+        deleteClock={deleteClock}
+      />
+    );
 
-  it("should update clock title on input change", () => {});
+    const editClockButton = screen.getByText("Edit Clock");
+    fireEvent.click(editClockButton);
 
-  it("should updates clock timezone on select change", () => {});
+    expect(screen.getByText("Edit Clock Form")).toBeInTheDocument();
+  });
+
+  it("should update clock title on input change", () => {
+    render(
+      <ClockActions
+        local={false}
+        clock={clock}
+        updateClock={updateClock}
+        createClock={createClock}
+        deleteClock={deleteClock}
+      />
+    );
+
+    const editClockButton = screen.getByText("Edit Clock");
+    fireEvent.click(editClockButton);
+
+    const titleInput = screen.getByLabelText("Title");
+    fireEvent.change(titleInput, { target: { value: "New Clock Title" } });
+
+    expect(updateClock).toHaveBeenCalledWith({
+      title: "New Clock Title",
+      timezone: "GMT",
+      offset: -690,
+    });
+  });
+
+  it("should update clock timezone on select change", () => {
+    render(
+      <ClockActions
+        local={false}
+        clock={clock}
+        updateClock={updateClock}
+        createClock={createClock}
+        deleteClock={deleteClock}
+      />
+    );
+
+    const editClockButton = screen.getByText("Edit Clock");
+    fireEvent.click(editClockButton);
+
+    const timezoneSelect = screen.getByLabelText("Timezone");
+    fireEvent.change(timezoneSelect, { target: { value: "PST" } });
+
+    expect(updateClock).toHaveBeenCalledWith({
+      title: "Test Clock",
+      timezone: "PST",
+      offset: -690,
+    });
+  });
 });
+
+// import React from "react";
+// import { render, screen, fireEvent } from "@testing-library/react";
+// import ClockActions from "./ClockActions";
+
+// jest.mock("../clock-form/ClockForm", () => {
+//   return jest.fn(({ type, id, name, value, handleClock, disabled }) => (
+//     <input
+//       type={type}
+//       id={id}
+//       name={name}
+//       value={value}
+//       handleClock={handleClock}
+//       disabled={disabled}
+//     />
+//   ));
+// });
+
+// describe("ClockActions", () => {
+//   const clock = {
+//     title: "Test Clock",
+//     timezone: "GMT",
+//     offset: -690,
+//   };
+
+//   const updateClock = jest.fn();
+//   const createClock = jest.fn();
+//   const deleteClock = jest.fn();
+
+//   it("should renders correctly when local is false", () => {
+//     render(
+//       <ClockActions
+//         local={false}
+//         clock={clock}
+//         updateClock={updateClock}
+//         createClock={createClock}
+//         deleteClock={deleteClock}
+//       />
+//     );
+
+//     expect(screen.getByText("Edit Clock")).toBeInTheDocument();
+//     expect(screen.getByText("Delete Folder")).toBeInTheDocument();
+//   });
+
+//   it("should renders correctly when local is true", () => {
+//     render(
+//       <ClockActions
+//         local={true}
+//         clock={clock}
+//         updateClock={updateClock}
+//         createClock={createClock}
+//         deleteClock={deleteClock}
+//       />
+//     );
+
+//     expect(screen.getByText("Edit Clock")).toBeInTheDocument();
+//     expect(screen.getByText("Create Folder")).toBeInTheDocument();
+//   });
+
+//   it("should toggles clock form when clicked edit clock button", () => {});
+
+//   it("should update clock title on input change", () => {});
+
+//   it("should updates clock timezone on select change", () => {});
+// });
