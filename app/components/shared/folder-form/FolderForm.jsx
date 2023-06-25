9b61444380fb759b69f03d6fb4f415d790efd418
Mohosin999 <programmermohosin@gmail.com>
2023-06-25 08:25:13 +0600

    folder form works successfully

--- app/components/shared/folder-form/FolderForm.jsx
+++ app/components/shared/folder-form/FolderForm.jsx
@@ -3,7 +3,14 @@ import React, { useState } from "react";
 const FolderForm = () => {
   const [folderName, setFolderName] = useState("");
   const [password, setPassword] = useState("");
+  const [step, setStep] = useState(1);
 
+  // This function is for step management
+  const handleNextStep = () => {
+    setStep(step + 1);
+  };
+
+  // This function is for performing all tasks after from submit
   const handleCreateFolder = (e) => {
     e.preventDefault();
 
@@ -13,24 +20,38 @@ const FolderForm = () => {
     // Clear form inputs
     setFolderName("");
     setPassword("");
+    setStep(1);
   };
 
   return (
-    <form onSubmit={handleCreateFolder}>
-      <input
-        type="text"
-        placeholder="Folder Name"
-        value={folderName}
-        onChange={(e) => setFolderName(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button type="submit">Create Folder</button>
-    </form>
+    <div>
+      {step === 1 && (
+        <div>
+          <h2>Enter Folder Name</h2>
+          <input
+            type="text"
+            placeholder="Folder Name"
+            value={folderName}
+            onChange={(e) => setFolderName(e.target.value)}
+          />
+          <button onClick={handleNextStep}>Next</button>
+        </div>
+      )}
+
+      {step === 2 && (
+        <div>
+          <h2>Enter Password</h2>
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button onClick={handleCreateFolder}>Create Folder</button>
+          <button onClick={() => setStep(step - 1)}>Previous</button>
+        </div>
+      )}
+    </div>
   );
 };
 
