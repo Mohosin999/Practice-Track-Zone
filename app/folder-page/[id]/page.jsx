5d67319c45e1f43557acca807082c1a0ae932c4d
Mohosin999 <programmermohosin@gmail.com>
2023-07-09 11:16:34 +0600

    commit

--- app/folder-page/[id]/page.jsx
+++ app/folder-page/[id]/page.jsx
@@ -7,34 +7,12 @@ import ClockActions from "@/app/components/shared/clock-actions/ClockActions";
 // import ClockForm from "@/app/components/shared/clock-form/ClockForm";
 
 const FolderPage = () => {
-  const [text, setText] = useState("");
-  const [textList, setTextList] = useState([]);
   const router = useRouter();
   const pathName = usePathname();
 
-  const [isCreate, setIsCreate] = useState(false);
-
-  useEffect(() => {
-    const savedTextList = localStorage.getItem(pathName);
-    if (savedTextList) {
-      setTextList(JSON.parse(savedTextList));
-    }
-  }, []);
-
-  const handleAddText = () => {
-    const updatedTextList = [...textList, text];
-    localStorage.setItem(pathName, JSON.stringify(updatedTextList));
-    setTextList(updatedTextList);
-    setText("");
-  };
-
-  const handleCreateClock = () => {
-    setIsCreate(!isCreate);
-  };
-
   return (
     <div>
-      <h2>Welcome to Folder Page - {pathName}</h2>
+      <h2>Welcome to Folder Page - {pathName} </h2>
       <p>
         In the example above, we import the useRouter() hook from the
         next/router module. We then call useRouter() to get the router object,
@@ -55,3 +33,28 @@ const FolderPage = () => {
 };
 
 export default FolderPage;
+
+// const [text, setText] = useState("");
+// const [textList, setTextList] = useState([]);
+// const router = useRouter();
+// const pathName = usePathname();
+
+// const [isCreate, setIsCreate] = useState(false);
+
+// useEffect(() => {
+//   const savedTextList = localStorage.getItem(pathName);
+//   if (savedTextList) {
+//     setTextList(JSON.parse(savedTextList));
+//   }
+// }, []);
+
+// const handleAddText = () => {
+//   const updatedTextList = [...textList, text];
+//   localStorage.setItem(pathName, JSON.stringify(updatedTextList));
+//   setTextList(updatedTextList);
+//   setText("");
+// };
+
+// const handleCreateClock = () => {
+//   setIsCreate(!isCreate);
+// };
