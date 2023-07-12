c2f6011d5a2b26dd53a0df69d600bca61f28acf5
Mohosin999 <programmermohosin@gmail.com>
2023-07-12 19:28:10 +0600

    commit

--- app/components/folder-list/FolderLists.jsx
+++ app/components/folder-list/FolderLists.jsx
@@ -8,7 +8,7 @@ const FolderLists = ({ clocks, updateClock, deleteClock, localClock }) => {
     (actions) => actions.clockModel.setFolders
   );
 
-  const itemsPerPage = 10; // Number of folders to show per page
+  const itemsPerPage = 3; // Number of folders to show per page
   const [currentPage, setCurrentPage] = useState(1);
   const [displayedFolders, setDisplayedFolders] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
@@ -41,11 +41,23 @@ const FolderLists = ({ clocks, updateClock, deleteClock, localClock }) => {
     setCurrentPage(1);
   };
 
-  const totalPages = Math.ceil(displayedFolders.length / itemsPerPage);
+  const totalPages = Math.ceil(
+    folders.filter((folder) =>
+      folder.folderName.toLowerCase().includes(searchTerm.toLowerCase())
+    ).length / itemsPerPage
+  );
   const pageNumbers = Array.from(
     { length: totalPages },
     (_, index) => index + 1
   );
+
+  const goToPreviousPage = () => {
+    setCurrentPage((prevPage) => prevPage - 1);
+  };
+
+  const goToNextPage = () => {
+    setCurrentPage((prevPage) => prevPage + 1);
+  };
 
   return (
     <div>
@@ -72,6 +84,9 @@ const FolderLists = ({ clocks, updateClock, deleteClock, localClock }) => {
             />
           ))}
           <div>
+            <button onClick={goToPreviousPage} disabled={currentPage === 1}>
+              Previous
+            </button>
             {pageNumbers.map((pageNumber) => (
               <button
                 key={pageNumber}
@@ -81,6 +96,12 @@ const FolderLists = ({ clocks, updateClock, deleteClock, localClock }) => {
                 {pageNumber}
               </button>
             ))}
+            <button
+              onClick={goToNextPage}
+              disabled={currentPage === totalPages}
+            >
+              Next
+            </button>
           </div>
         </div>
       )}
@@ -89,117 +110,3 @@ const FolderLists = ({ clocks, updateClock, deleteClock, localClock }) => {
 };
 
 export default FolderLists;
-
-// import React, { useEffect, useState } from "react";
-// import FolderDisplay from "../shared/folder-display/FolderDisplay";
-// import { useStoreActions, useStoreState } from "easy-peasy";
-
-// const FolderLists = () => {
-//   const folders = useStoreState((state) => state.clockModel.folders);
-//   const setFolders = useStoreActions(
-//     (actions) => actions.clockModel.setFolders
-//   );
-
-//   const itemsPerPage = 10; // Number of folders to show per page
-//   const [currentPage, setCurrentPage] = useState(1);
-//   const [displayedFolders, setDisplayedFolders] = useState([]);
-
-//   useEffect(() => {
-//     const storedFolders = localStorage.getItem("folders");
-//     if (storedFolders) {
-//       setFolders(JSON.parse(storedFolders));
-//     }
-//   }, [setFolders]);
-
-//   useEffect(() => {
-//     const startIndex = (currentPage - 1) * itemsPerPage;
-//     const endIndex = startIndex + itemsPerPage;
-//     setDisplayedFolders(folders.slice(startIndex, endIndex));
-//   }, [folders, currentPage]);
-
-//   const handlePageChange = (pageNumber) => {
-//     setCurrentPage(pageNumber);
-//   };
-
-//   const totalPages = Math.ceil(folders.length / itemsPerPage);
-//   const pageNumbers = Array.from(
-//     { length: totalPages },
-//     (_, index) => index + 1
-//   );
-
-//   return (
-//     <div>
-//       <h3>All Folder Lists</h3>
-//       <hr />
-
-//       {folders.length === 0 ? (
-//         <p>There is no folder, please create one</p>
-//       ) : (
-//         <div>
-//           {displayedFolders.map((folder) => (
-//             <FolderDisplay
-//               key={folder.id}
-//               id={folder.id}
-//               folderName={folder.folderName}
-//             />
-//           ))}
-//           <div>
-//             {pageNumbers.map((pageNumber) => (
-//               <button
-//                 key={pageNumber}
-//                 onClick={() => handlePageChange(pageNumber)}
-//                 disabled={currentPage === pageNumber}
-//               >
-//                 {pageNumber}
-//               </button>
-//             ))}
-//           </div>
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default FolderLists;
-
-// import React, { useEffect } from "react";
-// import FolderDisplay from "../shared/folder-display/FolderDisplay";
-// import { useStoreActions, useStoreState } from "easy-peasy";
-
-// const FolderLists = ({ clocks, updateClock, deleteClock, localClock }) => {
-//   const folders = useStoreState((state) => state.clockModel.folders);
-//   const setFolders = useStoreActions(
-//     (actions) => actions.clockModel.setFolders
-//   );
-
-//   // Get created folder from local storage
-//   useEffect(() => {
-//     const storedFolders = localStorage.getItem("folders");
-//     if (storedFolders) {
-//       setFolders(JSON.parse(storedFolders));
-//     }
-//   }, [setFolders]);
-
-//   return (
-//     <div>
-//       <h3>All Folder Lists</h3>
-//       <hr />
-
-//       {folders.length === 0 ? (
-//         <p>There is no folder, please create one</p>
-//       ) : (
-//         <div>
-//           {folders.map((folder) => (
-//             <FolderDisplay
-//               key={folder.id}
-//               id={folder.id}
-//               folderName={folder.folderName}
-//             />
-//           ))}
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default FolderLists;
