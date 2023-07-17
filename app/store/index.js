aa26670515ea6a39772d01ccc7e6383810124da8
Mohosin999 <programmermohosin@gmail.com>
2023-07-17 08:03:24 +0600

    commit

--- app/store/index.js
+++ app/store/index.js
@@ -1,8 +1,10 @@
 import { createStore } from "easy-peasy";
 import clockModel from "./clock-model";
+import eventModel from "./event-model";
 
 const store = createStore({
   clockModel,
+  eventModel,
 });
 
 export default store;
